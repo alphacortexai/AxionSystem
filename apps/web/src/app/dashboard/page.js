b6a98b5dee2a5b2783e77b5e96f1ce3ad560523f
Edrine Mutebi <edrinemutebi7@gmail.com>
2025-12-17 16:36:09 +0300

    solve use client issue

--- apps/web/src/app/dashboard/page.js
+++ apps/web/src/app/dashboard/page.js
@@ -1,6 +1,4 @@
-\"use client\";
-
-import { useEffect } from 'react';
+"use client";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { useAuth } from '../../lib/auth-context';
